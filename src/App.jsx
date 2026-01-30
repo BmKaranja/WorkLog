5df516626f3f2d0022439f8f6614186b8c225d0f
Brian Karanja <bmkaranja001@gmail.com>
2026-01-30 17:42:12 +0300

    30-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { Link } from 'react-router'
+import { Link, Routes } from 'react-router'
 import './App.css'
 
 function App() {
-
   return (
-    <>
+    <div className='body'>
       <div className='text-6xl text-white banner'>
         <h1>WORK LOG</h1>
       </div>
       <main>
         <section className='dates'>
-          <article className='years'>
-            <Link to=''>2025</Link>
-            <Link to=''>2026</Link>
+          <article id='years'>
+            <Link to='' className='years'>2025</Link>
+            <Link to='' className='years'>2026</Link>
           </article>
           <article className='months'>
 
           </article>
         </section>
       </main>
-    </>
+    </div>
   )
 }
 
