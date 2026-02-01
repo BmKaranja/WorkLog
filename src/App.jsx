d0a4f3ebfc9aee857586953bb6ab21c02b140073
Brian Karanja <bmkaranja001@gmail.com>
2026-02-01 19:05:57 +0300

    01-02

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,23 @@ function App() {
       </div>
       <main>
         <section className='dates'>
-          <article id='years'>
-            <Link to='' className='years'>2025</Link>
-            <Link to='' className='years'>2026</Link>
+          <article id="years" >
+            <Link to='#2025' className='years'>2025</Link>
+            <Link to='32026' className='years'>2026</Link>
           </article>
-          <article className='months'>
-
+          <article id='months'>
+            <Link to='#Jan' className='months'>January</Link>
+            <Link to='#Feb' className='months'>February</Link>
+            <Link to ='#Mar' className='months'>March</Link>
+            <Link to='#Apr' className='months'>April</Link>
+            <Link to='May' className='months'>May</Link>
+            <Link to='#Jun' className='months'>June</Link>
+            <Link to='#Jul' className='months'>July</Link>
+            <Link to='#Aug' className='months'>August</Link>
+            <Link to='#Sep' className='months'>September</Link>
+            <Link to='#Oct' className='months'>October</Link>
+            <Link to='#Nov' className='months'>November</Link>
+            <Link to='#Dec'className='months'>December</Link>
           </article>
         </section>
       </main>
