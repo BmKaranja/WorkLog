5eac1c589e57c43c797d1d66164d0981f31b5219
Brian Karanja <bmkaranja001@gmail.com>
2026-02-02 09:51:25 +0300

    02-02

---
+++ src/Months.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Months() {
+  return (
+    <div id>
+      
+    </div>
+  )
+}
+
+export default Months
---
+++ src/Year.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+import Months from './Months'
+
+function year() {
+  return (
+    <div i>
+      <Months/>
+    </div>
+  )
+}
+
+export default year
